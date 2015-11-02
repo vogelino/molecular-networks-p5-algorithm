e8cfcd6dffc8d6958ac4142d081a7d4f7e7e7985
Lucas Vogel <contact@vogelino.ch>
2015-11-02 10:57:44 +0100

    Creating a full size canevas

--- sketch.js
+++ sketch.js
@@ -1,9 +1,7 @@
-var canvasWidth = window.width;
-var canvasHeight = window.height;
-// getting started with p5js
 function setup(){
     colorMode(HSL, 360, 100, 100, 100);
-    createCanvas(canvasWidth, canvasWidth);
+    createCanvas(document.body.clientWidth, document.body.clientHeight);
 }
+
 function draw(){
 }
