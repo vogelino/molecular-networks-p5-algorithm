4eb5304c02846f94b1411d48c7ecb046c1dc6924
Lucas Vogel <contact@vogelino.ch>
2015-11-11 12:29:15 +0100

    Third version of the algorithm v.003

--- src/sketch.js
+++ src/sketch.js
@@ -11,9 +11,10 @@ function setup() {
 	props = getDefaults();
 
 	background(props.backgroundColor);
-	for (var i = 0; i < random(20, 20); i++) {
-		generateEllipses();
-	}
+	generateGroups();
+	props.groups.forEach((group) => {
+		drawConnections(group);
+	});
 }
 
 function draw() {
@@ -22,70 +23,159 @@ function draw() {
 
 function getDefaults() {
 	return {
+		availableFormId: 0,
 		maxClusterDepth: 3,
-		ellipsesDistance: random(1, 15),
+		circlesDistance: random(1, 15),
+		circleMinSize: 10,
 		arcWidth: 3,
 		arcStrokeWeigth: 1,
 		defaults : {
-			ellipse: { x: 0, y: 0, size: 0, ellipseColor: getColor() }
+			circle: { x: 0, y: 0, size: 0, circleColor: getColor() }
 		},
 		backgroundColor: 'whitesmoke',
-		ellipses: []
+		groups: [],
+		wrapperSize: 50
 	};
 }
 
-function getExtremeArcPoints(firstEllipse, nextEllipse, dir) {
+function getExtremeArcPoints(firstCircle, nextCircle) {
 	let points = {};
-	points.start = firstEllipse.arcPoints[dir];
+	points.start = firstCircle.arcPoints[dir];
 
 	switch(dir) {
 		case 'right':
-			points.end = nextEllipse.arcPoints.left;
+			points.end = nextCircle.arcPoints.left;
 		break;
 		case 'left':
-			points.end = nextEllipse.arcPoints.right;
+			points.end = nextCircle.arcPoints.right;
 		break;
 		case 'top':
-			points.end = nextEllipse.arcPoints.bottom;
+			points.end = nextCircle.arcPoints.bottom;
 		break;
 		case 'bottom':
-			points.end = nextEllipse.arcPoints.top;
+			points.end = nextCircle.arcPoints.top;
 		break;
 	}
 
 	return points;
 }
 
-function drawEllipse(theEllipse) {
-	let { x, y, size, ellipseColor } = theEllipse;
-	noStroke();
-	fill(ellipseColor);
-	ellipse(x, y, size, size);
+function drawForm(wrapper) {
+	let { x, y, circle } = wrapper;
+	stroke('whitesmoke');
+	strokeWeight(1);
+	noFill();
+	rect(x, y, props.wrapperSize, props.wrapperSize);
+	if (circle) {
+		noStroke();
+		fill(circle.circleColor);
+		ellipse(circle.x, circle.y, circle.size, circle.size);
+	}
 }
 
-function drawConnections(baseEllipse, nextEllipse, dir) {
-	let { start, end } = getExtremeArcPoints(baseEllipse, nextEllipse, dir);
-	let { arcWidth, arcStrokeWeigth } = props;
-	noFill()
-	stroke(baseEllipse.ellipseColor);
-	strokeWeight(arcStrokeWeigth);
-	line(start.x, start.y, end.x, end.y);
+function drawConnections(group) {
+	let { columns, rows } = group;
+
+	group.forms.forEach((form, index) => {
+			let isFirstColumn = form.column === 1;
+			let isFirstRow = form.row === 1;
+			let isLastRow = form.row === rows;
+			let isLastColumn = form.column === columns;
+			let rightSibling, leftSibling, topSibling, bottomSibling;
+
+			if (!isFirstRow) {
+				topSibling = getFormByPosition(group, form.row - 1, form.column);
+				drawConnection('top', form, topSibling);
+			}
+			if (!isFirstColumn) {
+				leftSibling = getFormByPosition(group, form.row, form.column - 1);
+				drawConnection('left', form, leftSibling);
+			}
+			if (!isLastRow) {
+				bottomSibling = getFormByPosition(group, form.row + 1, form.column);
+				drawConnection('bottom', form, bottomSibling);
+			}
+			if (!isLastColumn) {
+				rightSibling = getFormByPosition(group, form.row, form.column + 1);
+				drawConnection('right', form, rightSibling);
+			}
+	});
 }
 
-function addElipse(x , y, size, ellipseColor) {
-	x = x ? x : props.defaults.ellipse.x,
-	y = y ? y : props.defaults.ellipse.x,
-	size = size ? size : props.defaults.ellipse.size,
-	ellipseColor = ellipseColor ? ellipseColor : props.defaults.ellipse.color
-	let ellipseCenter = size / 2;
-	let arcPoints = {
-		top: {x, y: y - ellipseCenter},
-		bottom: {x, y: y + ellipseCenter},
-		left: {x: x - ellipseCenter, y},
-		right: {x: x + ellipseCenter, y}
+function drawConnection(dir, startForm, endForm) {
+	if (!endForm) { debugger };
+	if (!startForm.circle || !endForm.circle) { return; }
+	let startPoint = {};
+	let endPoint = {};
+	console.log(startForm.circle.arcPoints, endForm.circle.arcPoints);
+	switch(dir) {
+		case 'right':
+			startPoint = startForm.circle.arcPoints.right;
+			endPoint = endForm.circle.arcPoints.left;
+		break;
+		case 'left':
+			startPoint = startForm.circle.arcPoints.left;
+			endPoint = endForm.circle.arcPoints.right;
+		break;
+		case 'top':
+			startPoint = startForm.circle.arcPoints.top;
+			endPoint = endForm.circle.arcPoints.bottom;
+		break;
+		case 'bottom':
+			startPoint = startForm.circle.arcPoints.bottom;
+			endPoint = endForm.circle.arcPoints.top;
+		break;
+	}
+
+	stroke(endForm.circle.circleColor);
+	strokeWeight(props.arcStrokeWeigth);
+	line(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+}
+
+function addForm(id, x , y, size, row, column) {
+	let hasCircle = Math.floor(random(0,3)) === 1;
+	let circleSize = random(props.circleMinSize, props.wrapperSize - 5);
+	let circleX = x + (props.wrapperSize / 2);
+	let circleY = y + (props.wrapperSize / 2);
+	let form = {
+		id,
+		x,
+		y,
+		row,
+		column,
+		circle: hasCircle ? false : {
+			x: circleX,
+			y: circleY,
+			size: circleSize,
+			circleColor: getColor()
+		}
 	};
-	props.ellipses.push({ x, y, size, ellipseColor, arcPoints });
-	return props.ellipses[props.ellipses.length - 1];
+	if (form.circle) {
+		let circleCenter = circleSize / 2;
+		form.circle.arcPoints = {
+			top: {x: circleX, y: circleY - circleCenter},
+			bottom: {x: circleX, y: circleY + circleCenter},
+			left: {x: circleX - circleCenter, y: circleY},
+			right: {x: circleX + circleCenter, y: circleY}
+		};
+	}
+	return form;
+}
+
+function getFormById(id) {
+	let allForms = [];
+	props.groups.forEach((group) => {
+		allForms.concat(group.forms);
+	});
+	return allForms.filter((form) => {
+		return form.id === id;
+	})[0];
+}
+
+function getFormByPosition(group, row, column) {
+	return group.forms.filter((form) => {
+		return form.row === row && form.column == column;
+	})[0];
 }
 
 function getColor(bri, hue, sat, alpha) {
@@ -96,83 +186,38 @@ function getColor(bri, hue, sat, alpha) {
 	return color(hue, sat, bri, alpha);
 }
 
-function generateEllipses() {
-	let startSize = random(10,100);
-	let randomPosX = random(0, width);
-	let randomPosY = random(0, height);
-	let startEllipse = addElipse(randomPosX, randomPosY, startSize, getColor());
-	drawEllipse(startEllipse);
+function generateGroups() {
+	let rows = Math.floor(random(2,10));
+	let columns = Math.floor(random(2,10));
+	let formPosX = Math.floor(random(1, 30)) * props.wrapperSize;
+	let formPosY = Math.floor(random(1, 15)) * props.wrapperSize;
+	let columnPosX = formPosX;
+	let rowPosY = formPosY;
+	let forms = [];
+	let groupId = props.groups.length;
 
-	createSiblings('top', 1, startEllipse);
-	createSiblings('bottom', 1, startEllipse);
-	createSiblings('right', 1, startEllipse);
-	createSiblings('left', 1, startEllipse);
-}
-
-function createSiblings(dir, level, baseEllipse) {
-	if (level >= random(1, 4)) { return; }
-
-	let {x, y, size, ellipseColor} = baseEllipse;
-	let newSize = size * 0.6;
-	let ellipsesDistance = random(props.ellipsesDistance - 5, props.ellipsesDistance + 5);
-	let newDistance = ellipsesDistance * level * 1.5;
-	let sibling;
-
-	if (dir === 'right') {
-		sibling = {
-			x: x + (size / 2) + newDistance + (newSize / 2),
-			y,
+	for (let row = 1; row <= rows; row++) {
+		for (let column = 1; column <= columns; column++) {
+			let form = addForm(
+				props.availableFormId,
+				columnPosX,
+				rowPosY,
+				props.wrapperSize,
+				row,
+				column
+			);
+			drawForm(form);
+			forms.push(form);
+			columnPosX = column === columns ? formPosX : columnPosX + props.wrapperSize;
+			props.availableFormId++;
 		};
-	}
-	else if (dir === 'left') {
-		sibling = {
-			x: x - (size / 2) - newDistance - (newSize / 2),
-			y
-		};
-	}
-	else if (dir === 'top') {
-		sibling = {
-			x,
-			y: y - (size / 2) - newDistance - (newSize / 2),
-		};
-	}
-	else if (dir === 'bottom') {
-		sibling = {
-			x,
-			y: y + (size / 2) + newDistance + (newSize / 2)
-		};
-	}
-	sibling.size = newSize;
-	sibling.ellipseColor = color(
-		baseEllipse.ellipseColor.getHue(),
-		baseEllipse.ellipseColor.getSaturation() - (level * 2),
-		baseEllipse.ellipseColor.getBrightness() + (level * 4),
-		baseEllipse.ellipseColor.getAlpha()
-	);
+		rowPosY = row === rows ? formPosY : rowPosY + props.wrapperSize;
+	};
 
-	sibling = addElipse(sibling.x, sibling.y, sibling.size, sibling.ellipseColor);
-	drawEllipse(sibling);
-	drawConnections(baseEllipse, sibling, dir);
-	switch(dir) {
-		case 'right':
-			createSiblings('top', level + 1, sibling);
-			createSiblings('right', level + 1, sibling);
-			createSiblings('bottom', level + 1, sibling);
-		break;
-		case 'top':
-			createSiblings('top', level + 1, sibling);
-			createSiblings('right', level + 1, sibling);
-			createSiblings('left', level + 1, sibling);
-		break;
-		case 'left':
-			createSiblings('top', level + 1, sibling);
-			createSiblings('bottom', level + 1, sibling);
-			createSiblings('left', level + 1, sibling);
-		break;
-		case 'bottom':
-			createSiblings('right', level + 1, sibling);
-			createSiblings('bottom', level + 1, sibling);
-			createSiblings('left', level + 1, sibling);
-		break;
-	}
+	props.groups.push({
+		id: props.groups.length,
+		rows: rows,
+		columns: columns,
+		forms: forms
+	});
 }
