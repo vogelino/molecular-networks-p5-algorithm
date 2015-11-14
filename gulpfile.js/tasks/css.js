443fede845693183cf54caaa606079833c7a6775
Lucas Vogel <contact@vogelino.ch>
2015-11-14 12:49:08 +0100

    Changed to stylus

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -3,10 +3,10 @@ if(!config.tasks.css) return
 
 var gulp         = require('gulp')
 var browserSync  = require('browser-sync')
-var sass         = require('gulp-sass')
+var stylus       = require('gulp-stylus')
 var sourcemaps   = require('gulp-sourcemaps')
 var handleErrors = require('../lib/handleErrors')
-var autoprefixer = require('gulp-autoprefixer')
+var nib          = require('nib')
 var path         = require('path')
 
 var paths = {
@@ -17,9 +17,10 @@ var paths = {
 var cssTask = function () {
   return gulp.src(paths.src)
     .pipe(sourcemaps.init())
-    .pipe(sass(config.tasks.css.sass))
+    .pipe(stylus({
+      use: nib()
+    }))
     .on('error', handleErrors)
-    .pipe(autoprefixer(config.tasks.css.autoprefixer))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
