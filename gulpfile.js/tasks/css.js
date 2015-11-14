5263df0e40ba7a8384a9cd91859c1b9b45be81d3
Lucas Vogel <contact@vogelino.ch>
2015-11-14 11:56:36 +0100

    Duplicated repo from https://github.com/vigetlabs/gulp-starter

---
+++ gulpfile.js/tasks/css.js
@@ -0,0 +1,29 @@
+var config       = require('../config')
+if(!config.tasks.css) return
+
+var gulp         = require('gulp')
+var browserSync  = require('browser-sync')
+var sass         = require('gulp-sass')
+var sourcemaps   = require('gulp-sourcemaps')
+var handleErrors = require('../lib/handleErrors')
+var autoprefixer = require('gulp-autoprefixer')
+var path         = require('path')
+
+var paths = {
+  src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
+  dest: path.join(config.root.dest, config.tasks.css.dest)
+}
+
+var cssTask = function () {
+  return gulp.src(paths.src)
+    .pipe(sourcemaps.init())
+    .pipe(sass(config.tasks.css.sass))
+    .on('error', handleErrors)
+    .pipe(autoprefixer(config.tasks.css.autoprefixer))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dest))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('css', cssTask)
+module.exports = cssTask
