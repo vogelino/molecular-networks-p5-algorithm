da33022ff2fbca6ab9e36080683d069716fde7b5
Lucas Vogel <contact@vogelino.ch>
2015-11-18 00:27:20 +0100

    Reset of the background color

--- src/javascripts/algorithm/defaults.js
+++ src/javascripts/algorithm/defaults.js
@@ -4,7 +4,7 @@ export default Immutable.Map({
 	groupsAmount: 1,
 	circleMinSize: 10,
 	lineWeight: 1,
-	backgroundColor: '#fff',
+	backgroundColor: '#f5f5f5',
 	wrapperSize: 30,
 	mainBrightness: 50,
 	mainColor: '#000',
